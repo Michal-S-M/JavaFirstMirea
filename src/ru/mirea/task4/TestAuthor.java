2fdc9c1c2c28e2bc1d201337d55aaf58a3e53169
Michal-S-M <mail.sedov.ms@gmail.vom>
2021-09-23 10:02:33 +0300

    test commit

--- src/ru/mirea/task4/TestAuthor.java
+++ src/ru/mirea/task4/TestAuthor.java
@@ -1,7 +1,7 @@
 package ru.mirea.task4;
 import java.util.Scanner;
 
-public class TestAuthor {
+public class TestAuthor{
     public static void main(String[] args) {
         Author a1 = new Author("Nik", "123@gmail.com", 'm' );
         System.out.println(a1);
