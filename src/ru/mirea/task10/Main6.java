619bea5636c88af8362ee6d558039cf9008d4099
Michal-S-M <mail.sedov.ms@gmail.vom>
2021-10-06 09:36:04 +0300

    test commit

--- src/ru/mirea/task10/Main6.java
+++ src/ru/mirea/task10/Main6.java
@@ -4,14 +4,13 @@ import java.util.Scanner;
 public class Main6 {
     public static void recursion(double n, double i) {
         if ((n==0)||(n==1)){
-            System.out.println("у данного числа нет делителя");
+            System.out.println("NO");
         }
         else if (i >= n) {
-            System.out.println("это все возможные делители");
+            System.out.println("YES");
         }
         else if (n%i==0) {
-            System.out.println(i+"; ");
-            recursion(n, i+1);
+            System.out.println("NO");
         }
         else {
             recursion(n, i+1);
@@ -21,7 +20,6 @@ public class Main6 {
         System.out.println("введите число: ");
         Scanner in = new Scanner(System.in);
         double n = in.nextDouble();
-        System.out.println("далители числа: ");
-        recursion(n, 1);
+        recursion(n, 2);
     }
 }
